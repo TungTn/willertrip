d466c1f6ac03a98b7c0f639cf514cc616cb4a2cc
Tran Tung <tran.tung1311@gmail.com>
2022-06-14 15:23:51 +0700

    Update header & footer components

---
+++ components/footer/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div>
+            <h1>Here is Common Footer</h1>
+        </div>
+    )
+}
---
+++ components/nav/navlink.js
@@ -0,0 +1,19 @@
+export const navLinks = [
+    { name: "Home", path: "/" },
+    {
+        name: "mobi bus",
+        path: "/mobi",
+    },
+    {
+        name: "Tour",
+        path: "/tour",
+    },
+    {
+        name: "Xe khách",
+        path: "/route",
+    },
+    {
+        name: "Khuyến mãi",
+        path: "/promotion",
+    },
+];
